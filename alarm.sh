29380054439fed41598f9fbfd4dddb0f19c1cbb4
Malcolm Ke Win <kexianbin@diyism.com>
2022-08-24 16:51:04 +0800

    Update alarm.sh

--- alarm.sh
+++ alarm.sh
@@ -10,7 +10,12 @@ serdev="/dev/ttyUSB0"
 if [ "$action" = 1 ]; then
     echo 'on'
     sudo /bin/bash -c "echo -n -e '$HEX_CODE_ON' > $serdev"
-else
+elif [ "$action" = 0 ]; then
     echo 'Off'
+    sudo /bin/bash -c "echo -n -e '$HEX_CODE_OFF' > $serdev"
+else
+    echo 'Click'
+    sudo /bin/bash -c "echo -n -e '$HEX_CODE_ON' > $serdev"
+    sleep 1
     sudo /bin/bash -c "echo -n -e '$HEX_CODE_OFF' > $serdev"
 fi
